3da58e54c5decca4ebaff893f18a6e492347889c
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-07-24 14:49:55 -0300

    teste

---
+++ lista-exercicio-estrutura-de-repeticao/questoes/teste.js
@@ -0,0 +1,7 @@
+// let index = 0;
+// index < 10;
+// index ++;
+for (var index = 0; index < 10; index++) {
+    console.log("\u00EDndice de teste: ".concat(index));
+}
+console.log("Continuação do código...");
--- lista-exercicio-estrutura-de-repeticao/questoes/teste.ts
+++ lista-exercicio-estrutura-de-repeticao/questoes/teste.ts
@@ -1,1 +1,10 @@
-const teste : string = "teste";
+// let index = 0;
+// index < 10;
+// index ++;
+
+for (let index = 0; index < 10; index++) {
+    console.log(`índice de teste: ${index}`);
+    
+}
+
+console.log("Continuação do código...");
