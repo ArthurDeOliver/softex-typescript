05917251d5c33ca512707db8938190d52fc43ccc
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-08-25 23:42:22 -0300

    projeto completo

--- lista-exercicio-2/questoes/questao20.ts
+++ lista-exercicio-2/questoes/questao20.ts
@@ -2,6 +2,6 @@ var nome = prompt("digite seu nome");
 
 var pElement = document.getElementById("questao20");
 
-if(nome.length > 5){
+if (nome.length > 5) {
     pElement.textContent = `nome MAIOR que 5 caracteres`;
-}else pElement.textContent = `nome MENOR que 5 caracteres`;
+} else pElement.textContent = `nome MENOR que 5 caracteres`;
--- lista-exercicio-2/questoes/questao22.ts
+++ lista-exercicio-2/questoes/questao22.ts
@@ -1,7 +1,7 @@
 var alturaTriangulo = parseInt(prompt("coloque uma medida de uma altura de um triângulo"));
 var baseTriangulo = parseInt(prompt("coloque uma medida de uma base de um triângulo"));
 
-var areaTriangulo = (baseTriangulo*alturaTriangulo)/2;
+var areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
 
 var pElement = document.getElementById("questao22");
 
