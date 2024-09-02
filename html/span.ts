d6d18d0d362290bcd95dae75cf44be8f732da02a
ArthurDeOliver <arthurdeoliveiradev@gmail.com>
2024-09-02 16:56:36 -0300

    botao like melhorado

--- html/span.ts
+++ html/span.ts
@@ -1,12 +1,12 @@
 const span = document.getElementById("txt");
-//criação de elemento html
-const element = document.createElement("p"); //elemento do tipo parágrafo   
-element.textContent = "meu texto"; //adicionando texto a esse parágrafo
+// //criação de elemento html
+// const element = document.createElement("p"); //elemento do tipo parágrafo
+// element.textContent = "meu texto"; //adicionando texto a esse parágrafo
 
-document.body.appendChild(element); //adicionando no body o elemento criado
+// document.body.appendChild(element); //adicionando no body o elemento criado
 
 const textoPost = prompt("Escreva o texto do seu post aqui");
 
 span.textContent = textoPost;
 
-console.log(typeof(span));
+console.log(typeof span);
---
+++ html/test.js
@@ -0,0 +1,36 @@
+var isLiked = false; // variável dentro do arquivo
+//chamada sempre que executa o evento
+function like() {
+    isLiked = !isLiked;
+    //   const btnLike = document.getElementById("btn-like");
+    /*
+    ================= outra forma ===================
+      const button = document.getElementById("div-botao"); //div que contém o botão
+      const icon = button?.children[0].children[0];
+  
+    if (isLiked) {
+      icon.classList.remove("far");
+      icon.classList.add("fas");
+      icon.style.color = "red";
+    } else {
+      icon.classList.remove("fas");
+      icon.classList.add("far");
+      icon.style.color = "black";
+    }
+    
+    */
+    var iconHeart = document.getElementById("heart");
+    //   console.log(btnLike);
+    if (isLiked) {
+        iconHeart === null || iconHeart === void 0 ? void 0 : iconHeart.classList.remove("far");
+        iconHeart === null || iconHeart === void 0 ? void 0 : iconHeart.classList.add("liked");
+        iconHeart === null || iconHeart === void 0 ? void 0 : iconHeart.classList.add("fas");
+        iconHeart.style.color = "red";
+    }
+    else {
+        iconHeart === null || iconHeart === void 0 ? void 0 : iconHeart.classList.remove("fas");
+        iconHeart === null || iconHeart === void 0 ? void 0 : iconHeart.classList.remove("liked");
+        iconHeart === null || iconHeart === void 0 ? void 0 : iconHeart.classList.add("far");
+        iconHeart.style.color = "black";
+    }
+}
